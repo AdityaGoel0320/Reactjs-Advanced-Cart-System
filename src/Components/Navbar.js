b6429958413e62bc33be4c75ed4bb3c8c13f6295
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-06-13 01:34:39 +0530

    13/06/23

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
 
     return (
         <>
-            <div  className='flex justify-between items-center m-1 p-5 sticky top-0 w-full left-0 bg-opacity-50 '>
+            <div  className=' bg-black text-white flex justify-between items-center m-1 p-5 sticky top-0 w-full left-0 bg-opacity-50 '>
 
                 <div className='text-2xl font-bold'>
                     <h1>Logo</h1>
