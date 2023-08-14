0cfb3b8dffeeda523b4dc96b1cd4ecd6ce68c238
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-15 02:01:04 +0530

    some updates

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React from 'react';
-// import './App.css';
-// import TechStack from './Components/TechStack';
-// import Navbar from './Components/Navbar';
-// import Intro from './Components/Intro';
-// import Footer from './Components/Footer';
-// import AboutMe from './Components/AboutMe';
-// import Projects from './Components/Projects';
-// import Achievements from './Components/Achievements';
-// import ContactMe from './Components/ContactMe';
-// import Link from './Components/Link';
 import Cart from './component/Cart';
 
 
 function App() {
   return (
     <>
-      {/* <Link /> */}
-      {/* <Navbar />
-      <Intro />
-      <AboutMe />
-      <TechStack />
-      <Projects />
-      <Achievements />
-      <ContactMe />
-      <Footer />
-
-      <Achievements/> */}
-
       <Cart/>
     </>
   );
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
